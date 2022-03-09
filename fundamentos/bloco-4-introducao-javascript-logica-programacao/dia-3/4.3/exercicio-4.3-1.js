409144f9fa194ac3139e1abce678925dd25d9348
Larissa Lima <larissagiovannat@gmail.com>
2022-03-09 17:28:28 -0300

    adiciona exercicios do dia

---
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-3/4.3/exercicio-4.3-1.js
@@ -0,0 +1,7 @@
+let fatorial = 1;
+let resultado = fatorial;
+
+for (var index = 1; i < fatorial; i++) {
+    resultado *= 1;
+}
+console.log(resultado);
---
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-3/4.3/exercicio-4.3-4.js
@@ -0,0 +1,5 @@
+let maiorPrimo = 0;
+
+for (let index = 0; index <= 50; index ++) {
+    
+}
