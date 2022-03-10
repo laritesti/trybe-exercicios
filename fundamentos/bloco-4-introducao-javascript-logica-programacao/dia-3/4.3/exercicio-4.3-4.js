4100ea5b238cef101807c8f6f766fc6d0c1cfa9a
Larissa Lima <larissagiovannat@gmail.com>
2022-03-10 12:13:11 -0300

    Adiciona exercicios 4-9

--- fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-3/4.3/exercicio-4.3-4.js
+++ fundamentos/bloco-4-introducao-javascript-logica-programacao/dia-3/4.3/exercicio-4.3-4.js
@@ -1,5 +1,15 @@
 let maiorPrimo = 0;
 
-for (let index = 0; index <= 50; index ++) {
-    
+for (let index = 0; index <= 50; index += 1) {
+  let primo = true;
+  for (let divisorAtual = 2; divisorAtual < index; divisorAtual += 1) {
+    if (index % divisorAtual === 0) {
+      primo = false;
+    }
+  }
+  if (primo) {
+    maiorPrimo = index;
+  }
 }
+
+console.log(maiorPrimo);
